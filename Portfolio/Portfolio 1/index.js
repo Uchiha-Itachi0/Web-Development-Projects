6771b7bc07e1cc1fa9ac9990776e6a67274185b9
Anubhav Shukla <anubhav008shukla@gmail.com>
2021-11-01 17:51:53 +0530

    Updated project section

--- Portfolio/Portfolio 1/index.js
+++ Portfolio/Portfolio 1/index.js
@@ -52,27 +52,14 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
-    breakpoints: {
-        // when window width is >= 320px
-        0: {
-            slidesPerView: 1,
-        },
-        // when window width is >= 640px
-        640: {
-            slidesPerView: 2,
-        },
-        1000: {
-            slidesPerView: 3
-        }
-    },
     pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+      el: ".swiper-pagination",
+      clickable: true,
     },
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
-});
+  });
 
 
