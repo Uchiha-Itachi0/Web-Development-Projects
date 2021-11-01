ace02b31e6aba7db801cc476279418be61d0ded8
Anubhav Shukla <anubhav008shukla@gmail.com>
2021-11-01 22:44:44 +0530

    Added contact section

--- Portfolio/Portfolio 1/index.js
+++ Portfolio/Portfolio 1/index.js
@@ -43,18 +43,19 @@ $(document).ready(function () {
 
     // owl carousel script
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        // nav:true,
-        responsive:{
-            0:{
-                items:1
+        loop: true,
+        margin: 10,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:2
+            700: {
+                items: 2
             },
-            1000:{
-                items:3
+            1000: {
+                items: 3
             }
         }
     })
