8e7371edda16cdc7b725eddc5b8514b441ec4e18
Anubhav Shukla <anubhav008shukla@gmail.com>
2021-11-01 21:10:39 +0530

    Updated project section

--- Portfolio/Portfolio 1/index.js
+++ Portfolio/Portfolio 1/index.js
@@ -42,26 +42,22 @@ $(document).ready(function () {
     })
 
     // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0: {
-                items: 1,
-                nav: false
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        // nav:true,
+        responsive:{
+            0:{
+                items:1
             },
-            600: {
-                items: 2,
-                nav: false
+            600:{
+                items:2
             },
-            1000: {
-                items: 3,
-                nav: false
+            1000:{
+                items:3
             }
         }
-    });
+    })
 
     $('.home-container .img-container').ripples({
         blurRadius: 20,
